04c5d0c74d97ef5bbae8a125a0a4b239f122b242
Dan Bulychev <36710816+Dabulychev@users.noreply.github.com>
2026-09-20 08:32:18 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ authenticator = stauth.Authenticate(
 )
 
 # Исправленная строчка вызова логина (используется параметр location)
-name, authentication_status, username = authenticator.login('Login')
+name, authentication_status, username = authenticator.login(location='main', fields={'Form name': 'Login'})
 
 if authentication_status == False:
     st.error('Неверное имя пользователя или пароль')
